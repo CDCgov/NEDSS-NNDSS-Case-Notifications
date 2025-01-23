93d3e1590f5b20e7a5650013e1a6d4ad6cd04660
TeaSmith7 <137535421+TeaSmith7@users.noreply.github.com>
2025-01-23 11:00:23 -0500

    Add logger and use ApplicationRunner and apply constructor injection to start KafkaConsumerService - Added logger to the entry point NotificationProcessorApplication, KafkaConsumerService as well as updated pplication.properties with default log level - Using ApplicationRunner that ensures that kafkaConsumerService is automatically started when application starts.

--- notification-processor-service/src/main/java/gov/cdc/notificationprocessor/NotificationProcessorApplication.java
+++ notification-processor-service/src/main/java/gov/cdc/notificationprocessor/NotificationProcessorApplication.java
@@ -1,14 +1,32 @@
 package gov.cdc.notificationprocessor;
 
+import gov.cdc.notificationprocessor.consumer.KafkaConsumerService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationProcessorApplication {
+public class NotificationProcessorApplication implements ApplicationRunner {
+
+    private final KafkaConsumerService kafkaConsumerService;
+    private static final Logger logger = LoggerFactory.getLogger(NotificationProcessorApplication.class);
+
+    public NotificationProcessorApplication(KafkaConsumerService kafkaConsumerService) {
+        this.kafkaConsumerService = kafkaConsumerService;
+    }
 
     public static void main(String[] args) {
         //This is the main entry point where Spring Boot application is launched
         SpringApplication.run(NotificationProcessorApplication.class, args);
+        logger.info("Notification Processor Application Started");
+    }
+    @Override
+    public void run(ApplicationArguments args) {
+        kafkaConsumerService.listen();
     }
 
 }
--- notification-processor-service/src/main/java/gov/cdc/notificationprocessor/consumer/KafkaConsumerService.java
+++ notification-processor-service/src/main/java/gov/cdc/notificationprocessor/consumer/KafkaConsumerService.java
@@ -1,9 +1,25 @@
 package gov.cdc.notificationprocessor.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
 /**
  * KafkaConsumerService listens to a kafka topic and processes notifications it receives.
  * TODO - More detail to follow.
  */
+@Service
 public class KafkaConsumerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
+
+    public void listen() {
+        //TODO- reading from kafka topic, below code assumes xml is ready for processing
+        logger.info("Processing notification routes for STD/NonSTD for now manual");
+
+        //check if its std or non std and call handler accordingly.
+
+
+    }
 
 }

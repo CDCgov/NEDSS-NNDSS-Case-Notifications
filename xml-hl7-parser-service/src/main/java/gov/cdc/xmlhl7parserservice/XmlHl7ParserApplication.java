037abc787c4e41c2d887386b6b1b914423242903
Ragul Shanmugam <rshanmugam@enquizit.com>
2025-04-11 10:40:18 -0400

    Adding a xml hl7 parser helper library

---
+++ xml-hl7-parser-service/src/main/java/gov/cdc/xmlhl7parserservice/XmlHl7ParserApplication.java
@@ -0,0 +1,13 @@
+package gov.cdc.xmlhl7parserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlHl7ParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlHl7ParserApplication.class, args);
+    }
+
+}
---
+++ xml-hl7-parser-service/src/main/java/gov/cdc/xmlhl7parserservice/repository/INbsMsgouteRepository.java
@@ -0,0 +1,7 @@
+package gov.cdc.xmlhl7parserservice.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface INbsMsgouteRepository {
+}

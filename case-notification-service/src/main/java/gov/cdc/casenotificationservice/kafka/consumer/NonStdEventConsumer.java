c50a8e9d83701a2b4856c34d39e9defb317668ef
Duc Nguyen <dnguyen@enquizit.com>
2025-04-16 09:49:50 -0700

    update kafka consumer config

--- case-notification-service/src/main/java/gov/cdc/casenotificationservice/kafka/consumer/NonStdEventConsumer.java
+++ case-notification-service/src/main/java/gov/cdc/casenotificationservice/kafka/consumer/NonStdEventConsumer.java
@@ -1,28 +1,20 @@
 package gov.cdc.casenotificationservice.kafka.consumer;
 
-import gov.cdc.casenotificationservice.service.std.interfaces.IXmlService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StdEventConsumer {
-
+public class NonStdEventConsumer {
     private static final Logger logger = LoggerFactory.getLogger(StdEventConsumer.class); //NOSONAR
-    private final IXmlService xmlService;
-
-    public StdEventConsumer(IXmlService xmlService) {
-        this.xmlService = xmlService;
-    }
-
     @KafkaListener(
-            topics = "${spring.kafka.topic.std-topic}",
-            containerFactory = "kafkaListenerContainerFactoryConsumerForStd"
+            topics = "${spring.kafka.topic.non-std-topic}",
+            containerFactory = "kafkaListenerContainerFactoryConsumerForNonStd"
     )
     public void handleMessage(String message){
         try {
-            xmlService.mappingXmlStringToObject(message);
+
         } catch (Exception e) {
             logger.error("KafkaEdxLogConsumer.handleMessage: {}", e.getMessage());
         }

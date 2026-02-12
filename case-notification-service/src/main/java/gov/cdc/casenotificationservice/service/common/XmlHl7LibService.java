a15f3d2d5bfc36a70a95ac38e54875a3f7b11f6f
Tim Ballard <1425377+timoballard@users.noreply.github.com>
2026-02-12 07:11:08 -0600

    rm xml-hl7-parser-service

--- case-notification-service/src/main/java/gov/cdc/casenotificationservice/service/common/XmlHl7LibService.java
+++ case-notification-service/src/main/java/gov/cdc/casenotificationservice/service/common/XmlHl7LibService.java
@@ -1,15 +1,12 @@
 package gov.cdc.casenotificationservice.service.common;
 
 import gov.cdc.casenotificationservice.exception.APIException;
-import gov.cdc.casenotificationservice.repository.odse.CNTraportqOutRepository;
 import gov.cdc.casenotificationservice.repository.odse.model.CNTransportqOut;
 import gov.cdc.casenotificationservice.service.common.interfaces.IXmlHl7Service;
 import gov.cdc.xmlhl7parserlib.helper.HL7MessageBuilder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 @Service
-@ConditionalOnProperty(name = "xmlhl7.parser.mode", havingValue = "library", matchIfMissing = true)
 public class XmlHl7LibService implements IXmlHl7Service {
   private final HL7MessageBuilder hl7MessageBuilder;
 

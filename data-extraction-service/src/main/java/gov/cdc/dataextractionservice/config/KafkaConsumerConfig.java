250ade71384234adf778e517c26b9d16fcbf7e58
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-02-18 17:54:42 -0500

    test: stress test CI

--- data-extraction-service/src/main/java/gov/cdc/dataextractionservice/config/KafkaConsumerConfig.java
+++ data-extraction-service/src/main/java/gov/cdc/dataextractionservice/config/KafkaConsumerConfig.java
@@ -32,7 +32,7 @@ public class KafkaConsumerConfig {
   private Map<String, Object> baseConsumerConfigs(String groupId) {
     Map<String, Object> config = new HashMap<>();
     config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-    config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+      config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
     config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
     config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
     config.put(

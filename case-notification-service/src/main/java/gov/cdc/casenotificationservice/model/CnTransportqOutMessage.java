61aa6f4a91d0f047fc6bc95ec7ce796253197eda
Ada Ball <ada@adamball.org>
2026-04-27 14:57:36 -0500

    Move code files over from data-extraction-service
    These are all of the files that can be moved as-is with little modification.

---
+++ case-notification-service/src/main/java/gov/cdc/casenotificationservice/model/CnTransportqOutMessage.java
@@ -0,0 +1,10 @@
+package gov.cdc.casenotificationservice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CnTransportqOutMessage {
+  public EnvelopePayload payload;
+}
---
+++ case-notification-service/src/test/java/gov/cdc/casenotificationservice/service/cntransportqout/UpdateServiceTests.java
@@ -0,0 +1,49 @@
+package gov.cdc.casenotificationservice.service.cntransportqout;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class UpdateServiceTests {
+
+  @Mock private JdbcTemplate jdbcTemplate;
+
+  @InjectMocks private UpdateService service;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void testUpdateRecordStatus_Success() {
+    long uid = 100L;
+    String status = "COMPLETED";
+
+    when(jdbcTemplate.update(anyString(), eq(status), eq(uid))).thenReturn(1);
+
+    service.updateRecordStatus(uid, status);
+
+    verify(jdbcTemplate).update(anyString(), eq(status), eq(uid));
+  }
+
+  @Test
+  void testUpdateRecordStatus_NoRowsAffected() {
+    long uid = 101L;
+    String status = "FAILED";
+
+    when(jdbcTemplate.update(anyString(), eq(status), eq(uid))).thenReturn(0);
+
+    service.updateRecordStatus(uid, status);
+
+    verify(jdbcTemplate).update(anyString(), eq(status), eq(uid));
+  }
+}
